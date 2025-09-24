42881d4d7f505058b1caba2fe5c98d51db7c0403
MohammedsahadCP <mohammedsahad69@gmail.com>
2025-09-25 00:30:45 +0530

    some files are added in tasks

---
+++ tasks/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'description', 'status', 'user', 'created_at', 'updated_at')
+        read_only_fields = ('user', 'created_at', 'updated_at')
+
+    def create(self, validated_data):
+        # automatically assign task to the logged-in user
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
+from accounts.permissions import IsAdminOrOwner
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrOwner]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'admin':
+            return Task.objects.all()
+        # normal user sees only their own tasks
+        return Task.objects.filter(user=user)
