8b7cec0aad7f1174d92df54a5c26f151cb2d4c4a
MohammedsahadCP <mohammedsahad69@gmail.com>
2025-09-25 00:40:30 +0530

    updated file serializers in  task

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,10 +4,10 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'status', 'user', 'created_at', 'updated_at')
-        read_only_fields = ('user', 'created_at', 'updated_at')
+        fields = ['id', 'title', 'description', 'status', 'user', 'created_at', 'updated_at']
+        read_only_fields = ['user', 'created_at', 'updated_at']
 
     def create(self, validated_data):
-        # automatically assign task to the logged-in user
+        # Automatically assign the task to the logged-in user
         validated_data['user'] = self.context['request'].user
         return super().create(validated_data)
