1cd97c79aeea90ecbab0dfb65b93b8d8869038c8
MohammedsahadCP <mohammedsahad69@gmail.com>
2025-09-25 00:55:47 +0530

    changed views.py

--- tasks/views.py
+++ tasks/views.py
@@ -2,16 +2,15 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
-from accounts.permissions import IsAdminOrOwner
+from accounts.permissions import IsAdminOrOwner  
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated, IsAdminOrOwner]
 
     def get_queryset(self):
         user = self.request.user
         if user.role == 'admin':
-            return Task.objects.all()
-        # normal user sees only their own tasks
-        return Task.objects.filter(user=user)
+            return Task.objects.all()   
+        return Task.objects.filter(user=user) 
+
