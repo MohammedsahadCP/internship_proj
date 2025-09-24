14ce72d186c3efa9958f47586e98e137dade395b
MohammedsahadCP <mohammedsahad69@gmail.com>
2025-09-25 00:07:38 +0530

    updated url

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/accounts/',include('account.urls')),
+    path('api/accounts/',include('accounts.urls')),
 ]
