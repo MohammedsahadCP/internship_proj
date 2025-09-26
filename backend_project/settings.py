414affdb434f1e91dd2d95b4843ea710a88a5a2b
MohammedsahadCP <mohammedsahad69@gmail.com>
2025-09-27 01:29:36 +0530

    minor update

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -21,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = config("SECRET_KEY", default=get_random_secret_key())
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
